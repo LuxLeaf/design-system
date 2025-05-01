ab3e800ab24a32772d3daa210a02f70302868741
Armando Vasquez <mANdo2@mcnuggies.dev>
2025-05-01 14:30:39 -0500

    commit A

--- src/stories/Header.tsx
+++ src/stories/Header.tsx
@@ -18,6 +18,9 @@ export const Header = ({ user, onLogin, onLogout, onCreateAccount }: HeaderProps
   <header>
     <div className="storybook-header">
       <div>
+        <span>Some new content</span>
+      </div>
+      <div>
         <svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
           <g fill="none" fillRule="evenodd">
             <path
