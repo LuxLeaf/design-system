802a0f21236fd0de98eaf92b8e1615d83fa7e57c
Armando Vasquez <mANdo2@mcnuggies.dev>
2025-05-02 11:48:51 -0500

    area tag fix

--- src/stories/Header.tsx
+++ src/stories/Header.tsx
@@ -19,7 +19,7 @@ export const Header = ({ user, onLogin, onLogout, onCreateAccount }: HeaderProps
     <div className="storybook-header">
       <div>
         <body aria-hidden="true">Some body</body>
-        <area>No alt area</area>
+        <area/>
         <svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
           <g fill="none" fillRule="evenodd">
             <path
