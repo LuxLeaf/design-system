cb2a495f39e428b4388a72d09da40f6a6470a866
Armando Vasquez <mando@mcnuggies.dev>
2026-02-24 11:57:53 -0600

    test changes

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -25,13 +25,16 @@ export const Button = ({
 }: ButtonProps) => {
   const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
   return (
-    <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={{ backgroundColor }}
-      {...props}
-    >
-      {label}
-    </button>
+    <>
+      <button
+        type="button"
+        className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+        style={{ backgroundColor }}
+        {...props}
+      >
+        {label}
+      </button>
+      <p>A wild p tag appears!</p>
+    </>
   );
 };
