2577a5e2a6b36e330fafdb2997ece8e4b1bddc0d
Armando Vasquez <mando@mcnuggies.dev>
2026-03-06 11:54:39 -0600

    add story with OAuth string as prefix

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -38,6 +38,12 @@ export const Secondary: Story = {
   },
 };
 
+export const OAuthButton: Story = {
+  args: {
+    label: 'OAuth Button',
+  },
+};
+
 export const SecondaryOAuthButton: Story = {
   args: {
     label: 'OAuth Button',
