227b86b768511ad98753da4df371c0fd4454d469
Armando Vasquez <mando@mcnuggies.dev>
2026-07-02 14:21:12 -0500

    visual change

--- src/stories/Page.tsx
+++ src/stories/Page.tsx
@@ -69,7 +69,7 @@ export const Page: React.FC = () => {
         </div>
         <>bananas</>
         <>test</>
-        <p>Boopers</p>
+        <p>Booperssdfsdf</p>
       </section>
     </article>
   );
