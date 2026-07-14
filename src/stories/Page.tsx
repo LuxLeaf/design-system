b8271996d6427d7d66afcbbe1f06b17469820610
Armando Vasquez <mando@mcnuggies.dev>
2026-07-14 16:14:34 -0500

    direct commit visual change

--- src/stories/Page.tsx
+++ src/stories/Page.tsx
@@ -69,7 +69,7 @@ export const Page: React.FC = () => {
         </div>
         <>bananas</>
         <>test</>
-        <p>Boop</p>
+        <p>Boop visual change</p>
       </section>
     </article>
   );
