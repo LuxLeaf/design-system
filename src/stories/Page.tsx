813fa96d5e90b8f38c682ebbf60323d0c1093955
Armando Vasquez <mando@mcnuggies.dev>
2026-07-21 18:39:35 -0500

    visual change

--- src/stories/Page.tsx
+++ src/stories/Page.tsx
@@ -69,7 +69,7 @@ export const Page: React.FC = () => {
         </div>
         <>bananas</>
         <>test</>
-        <p>Boop visual change</p>
+        <p>Boop visual change asdfasdfasd</p>
       </section>
     </article>
   );
